9bdd395514b8c4e94a447d9c87600563b9488185
Anuradha Kamble <kambleanuradha89@gmail.com>
2026-07-01 23:28:11 +0530

    Initial Commit

---
+++ src/main/java/BrowserCommands.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserCommands {
+
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		WebDriver driver=  new ChromeDriver();
+		driver.get("https://www.google.com");
+	System.out.println(driver.getTitle());
+	System.out.println(driver.getCurrentUrl());
+	
+	driver.quit();
+		
+
+	}
+
+}
